c907a3ea4530059cb68e673f448a7cbbe463ef2d
michalantolik <michal.antolik@gmail.com>
2023-10-30 14:35:29 +0100

    Update DB service interface

--- Midas/MidasRatesUpdater/Services/Database/IDatabaseService.cs
+++ Midas/MidasRatesUpdater/Services/Database/IDatabaseService.cs
@@ -1,10 +1,18 @@
-﻿namespace MidasRatesUpdater.Services.Database
+﻿using Microsoft.EntityFrameworkCore;
+using MidasRatesUpdater.Services.Database.Entities;
+
+namespace MidasRatesUpdater.Services.Database
 {
     /// <summary>
     /// Service for accessing database.
     /// </summary>
-    public interface IDatabaseService
+    public interface IDatabaseService : IDisposable
     {
+        /// <summary>
+        /// DB table with currency exchange rates.
+        /// </summary>
+        DbSet<ExchangeRate> ExchangeRates { get; set; }
+
         /// <summary>
         /// Persists database changes.
         /// </summary>
