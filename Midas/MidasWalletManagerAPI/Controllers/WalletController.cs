f57ae2aa8d2a7df72e0943b6a6ad16e46fd9e4e4
michalantolik <michal.antolik@gmail.com>
2023-11-01 12:09:27 +0100

    Add MidasWalletManagerAPI project (Web API)

---
+++ Midas/MidasWalletManagerAPI/Controllers/WalletController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MidasWalletManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : ControllerBase
+    {
+        // TODO
+    }
+}
